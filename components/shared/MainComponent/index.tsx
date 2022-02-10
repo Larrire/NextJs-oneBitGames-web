358b4baaf21491e00a17611ed12f9119c0c6d175
Larrire <linekerlarrire@gmail.com>
2022-02-09 22:33:56 -0300

    Creating footer

--- components/shared/MainComponent/index.tsx
+++ components/shared/MainComponent/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from '../Header/StoreFrontHeader';
+import Footer from '../Footer/StoreFrontFooter';
 
 const MainComponent: React.FC = ({children}) => {
   return (
@@ -10,6 +11,7 @@ const MainComponent: React.FC = ({children}) => {
         {children}
       </div>
 
+      <Footer />
     </div>
   )
 }
