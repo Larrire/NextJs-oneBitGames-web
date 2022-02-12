cf21f8f2e860fe837d4f65ef51ecfcdb677f4a7b
Larrire <linekerlarrire@gmail.com>
2022-02-11 21:05:58 -0300

    Creating admin header

---
+++ components/shared/AdminComponent/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import AdminHeader from "../Header/AdminHeader";
+import { Row, Col } from 'react-bootstrap';
+
+const AdminComponent: React.FC = ({children}) => {
+  return (
+    <Row className="mr-lg-4">
+      <Col lg={3}>
+        Menu Lateral
+      </Col>
+
+      <Col lg={9}>
+        <div className="d-flex flex-column sticky-footer-wrapper container">
+          <AdminHeader name="Larrire" />
+
+          <div className="flex-fill text-center">
+            {children}
+          </div>
+        </div>
+      </Col>
+    </Row>
+  )
+}
